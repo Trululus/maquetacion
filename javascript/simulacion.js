8e66bf58f561772a375c844da87be9c76e22cc7c
leyton0921 <leytoncarmonaisabella@gmail.com>
2024-05-21 11:42:26 -0500

    .

--- javascript/simulacion.js
+++ javascript/simulacion.js
@@ -1,16 +1,23 @@
 let currentValue = 0;
-const goalValue = 100; // Meta monetaria
+        const goalValue = 100; // Meta monetaria
 
-function addMoney() {
-    // Simulamos agregar una cantidad aleatoria de dinero
-    const addedValue = Math.floor(Math.random() * 20) + 1;
-    currentValue += addedValue;
-    
-    // Actualizamos el contenido del elemento en el DOM
-    document.querySelector("#current-value").textContent = currentValue;
-    
-}
+        function addDonation() {
+            const donationAmountInput = document.getElementById('donation-amount');
+            const donationAmount = parseFloat(donationAmountInput.value);
+            const nombreInput = document.getElementById('username');
 
+            if (!isNaN(donationAmount) && donationAmount > 0) {
+                currentValue += donationAmount;
+
+                document.querySelector("#current-value").textContent = currentValue;
+
+                // Limpia el valor del campo de donación después de agregarlo
+                donationAmountInput.value = '';
+                nombreInput.value = '';
+            } else {
+                alert("Por favor, ingresa un valor válido para la donación.");
+            }
+        }
 
 
  
